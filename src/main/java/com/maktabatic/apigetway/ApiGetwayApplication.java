6adaffddff6af1c2b9a3c812102ad9378ca88c3b
Nour Elhouda Ben Saadi <nh.bensaadi@esi-sba.dz>
2021-08-27 21:39:59 +0100

    first

---
+++ src/main/java/com/maktabatic/apigetway/ApiGetwayApplication.java
@@ -0,0 +1,15 @@
+package com.maktabatic.apigetway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+
+@SpringBootApplication
+public class ApiGetwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGetwayApplication.class, args);
+	}
+
+}
